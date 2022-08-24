b4f0a3d040314296f9dc9915b0adaaed259af1d2
Vlad Brad <vbrad@beyondtrust.com>
2022-08-23 21:03:28 -0400

    [BTP-1232] Add a postinstall script to the installation package which runs the app, passing some arguments.

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 xcodebuild -workspace Installer_PoC.xcworkspace -scheme "HelloApp (macOS)" -configuration Release
 
 # Create installer packages.
-pkgbuild --identifier com.bomgar.HelloApp --component Build/Products/Release/HelloApp.app --install-location /Applications Build/HelloApp.pkg
+pkgbuild --identifier com.bomgar.HelloApp --component Build/Products/Release/HelloApp.app --scripts HelloApp/Scripts --install-location /Applications Build/HelloApp.pkg
