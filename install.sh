1bb640a63bf6e0bf2bc6f82479b6efb9fb0ec2b3
Vlad Brad <vbrad@beyondtrust.com>
2022-08-24 22:03:22 -0400

    [BTP-1232] Another possible way to pass a value to the postinstall script and the app.

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
 # A somewhat kludgy way of passing a value to the postinstall script, using launchctl's setenv/getenv/unsetenv commands.
-sudo sh -c 'launchctl setenv HELLO_APP_ARG $(uuidgen); installer -pkg Build/HelloApp.pkg -target /; launchctl unsetenv HELLO_APP_ARG'
+#sudo sh -c 'launchctl setenv HELLO_APP_ARG $(uuidgen); installer -pkg Build/HelloApp.pkg -target /; launchctl unsetenv HELLO_APP_ARG'
+
+# Another, possibly better way of passing a value to the postinstall script via the package's file name.
+
+PKG_SESSION_NAME=Build/HelloApp_$(uuidgen).pkg
+cp Build/HelloApp.pkg $PKG_SESSION_NAME
+
+sudo installer -pkg $PKG_SESSION_NAME -target /
+
+rm $PKG_SESSION_NAME
